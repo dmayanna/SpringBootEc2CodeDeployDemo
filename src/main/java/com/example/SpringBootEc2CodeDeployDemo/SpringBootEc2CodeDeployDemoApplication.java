936a8af9d62c353da426e7261861c6709cd42d88
dmayanna <smdeekshith@gmail.com>
2020-10-02 19:21:29 -0600

    pushing empty project

---
+++ src/main/java/com/example/SpringBootEc2CodeDeployDemo/SpringBootEc2CodeDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootEc2CodeDeployDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEc2CodeDeployDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEc2CodeDeployDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootEc2CodeDeployDemo/SpringBootEc2CodeDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootEc2CodeDeployDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEc2CodeDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
